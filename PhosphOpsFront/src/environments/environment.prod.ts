9b24dd3a725ddc9dd6ea42c32853d0ebaf0daa65
Malak-Messaoui <Malak.Messaoui@esprit.tn>
2026-09-03 20:08:52 +0100

    fix: add /PhosphOps context-path to prod apiUrl

--- PhosphOpsFront/src/environments/environment.prod.ts
+++ PhosphOpsFront/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://phosphopsbackend-bac3czavgtbea6aw.swedencentral-01.azurewebsites.net'
+  apiUrl: 'https://phosphopsbackend-bac3czavgtbea6aw.swedencentral-01.azurewebsites.net/PhosphOps'
 };
